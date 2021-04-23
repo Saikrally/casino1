44b067ebe67d6745700b02b17cd5b632ac865801
Saikrally <saikrallyshaman@gmail.com>
2021-04-23 16:01:03 +0300

    rename of test file

---
+++ test_login_page.py
@@ -0,0 +1,8 @@
+link = "https://www.tempspins.pw/en"
+
+
+def test_open_login_page(browser):
+    browser.get(link)
+    browser.implicitly_wait(5)
+    login_button = browser.find_element_by_class_name("btn hoverable cta-primary size-primary uppercase bold flex")
+    assert login_button.is_displayed(), "Login button isn't displayed"

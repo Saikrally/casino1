4975e9a525e1299c3c0c8648f3406e4d6fc7a3d9
Saikrally <saikrallyshaman@gmail.com>
2021-04-23 16:45:03 +0300

    assert login button is present

--- test_login_page.py
+++ test_login_page.py
@@ -3,6 +3,11 @@ link = "https://finnplay:password@www.tempspins.pw/"
 
 def test_open_login_page(browser):
     browser.get(link)
+    browser.maximize_window()
     browser.implicitly_wait(5)
-    login_button = browser.find_element_by_class_name("btn hoverable cta-primary size-primary uppercase bold flex")
+    login_button = browser.find_element_by_css_selector("body > app-root > app-common-layout > mat-sidenav-container "
+                                                        "> mat-sidenav > div.mat-drawer-inner-container.ng-tns-c94-0 "
+                                                        "> app-sidebar-content > div > app-desk-head-sidebar > div > "
+                                                        "div.logged-out-content.flex-content-c.ng-tns-c104-17.ng-star"
+                                                        "-inserted > div > app-button.login-btn.ng-tns-c104-17 > span")
     assert login_button.is_displayed(), "Login button isn't displayed"
